c26bb0d58845b8326913ae97de07329a749c8df1
Sasha <sasha.shvets.2017@mail.ru>
2023-11-01 11:27:11 +0200

    handling unsuccessful search

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,13 +1,17 @@
 import { Movie } from "./Movie";
 
 function Movies(props) {
-  const { movies } = props;
+  const { movies = [] } = props;
 
   return (
     <div className="movies">
-      {movies.map((movie) => {
-        return <Movie key={movie.imdbID} {...movie} />;
-      })}
+      {movies.length ? (
+        movies.map((movie) => {
+          return <Movie key={movie.imdbID} {...movie} />;
+        })
+      ) : (
+        <h3>Nothing found</h3>
+      )}
     </div>
   );
 }
